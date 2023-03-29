80de23fe6e7db8a41f5d6d6c9fc7e7197c14dbf7
oaemdl <oaemdl@gmail.com>
2023-03-29 16:48:52 -0500

    sts

---
+++ src/main/java/com/web/cinestar/WebCineStarStsApplication.java
@@ -0,0 +1,13 @@
+package com.web.cinestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCineStarStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCineStarStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/cinestar/WebCineStarStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.cinestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCineStarStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
